2d102c815f013980e87d44787f00134da7066009
Christian Weck <weck.chr@gmail.com>
2022-11-22 17:46:28 +0100

    Init: Third Commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Glück Auf! Ihr Zwei");
+        System.out.println("Glück Auf! Ihr Zwei und alle Anderen");
     }
 }
